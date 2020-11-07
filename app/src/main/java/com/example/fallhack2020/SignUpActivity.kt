9135b23c3d4778872575a83b21b81196b280a873
sachin <sraturi@sfu.ca>
2020-11-07 12:42:58 -0800

    added firebase

--- app/src/main/java/com/example/fallhack2020/SignUpActivity.kt
+++ app/src/main/java/com/example/fallhack2020/SignUpActivity.kt
@@ -2,8 +2,12 @@ package com.example.fallhack2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fallhack2020.model.SignUpForm
 
 class SignUpActivity : AppCompatActivity() {
+
+    private var signUpForm = SignUpForm()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
