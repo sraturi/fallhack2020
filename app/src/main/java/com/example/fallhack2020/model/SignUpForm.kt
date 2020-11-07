a1e1d8b5f4707b394667a6a793a640c0cafe00f0
sachin <sraturi@sfu.ca>
2020-11-07 14:37:02 -0800

    added signup process

--- app/src/main/java/com/example/fallhack2020/model/SignUpForm.kt
+++ app/src/main/java/com/example/fallhack2020/model/SignUpForm.kt
@@ -4,9 +4,11 @@ data class SignUpForm(var firstName:String = "") {
     var lastName:String = ""
     var phoneNumber:String = ""
     var dateOfBirth: String = ""
+    var email:String = ""
+    var password:String = ""
 
     fun isEverythingFilled(): Boolean {
-        if (firstName == "" || lastName == "" || phoneNumber== "" || dateOfBirth== "") {
+        if (firstName == "" || lastName == "" || phoneNumber== ""  || email ==""|| password == "") {
             return false
         }
         return true
