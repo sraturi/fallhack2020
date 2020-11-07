6240f2372494a3ecddc71183ab5b8ff92d431781
sachin <sraturi@sfu.ca>
2020-11-07 11:34:43 -0800

    setup fragments control

--- app/src/main/java/com/example/fallhack2020/MainActivity.kt
+++ app/src/main/java/com/example/fallhack2020/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.fallhack2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager2 = findViewById(R.id.viewPager2)
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager2.adapter = ViewPagerAdapter(this)
+
+        TabLayoutMediator(tabLayout, viewPager2) {
+                tab, position -> tab.text = ViewPagerAdapter.TABS[position]
+        }.attach()
     }
 }
